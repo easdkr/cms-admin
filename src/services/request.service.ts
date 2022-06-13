985d6ec091e5d6e54b7ab245effe1183976a979c
JUNE <choie0423@naver.com>
2022-06-13 16:01:44 +0900

    add axios instance

--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -1,8 +1,29 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { 
+    AxiosError,
+    AxiosInstance,
+    AxiosRequestConfig, 
+    AxiosResponse } 
+from "axios";
 
 
-const requestor: AxiosInstance = axios.create({
+const httpClient: AxiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_HOST,
     timeout: 10000
 })
 
+//request interceptor
+httpClient.interceptors.request.use((config: AxiosRequestConfig) => {
+    return config;
+});
+  
+//response interceptor
+httpClient.interceptors.response.use(
+    (response: AxiosResponse<any>) => {
+      return response;
+    },
+    (error: AxiosError & {config: {_retry: boolean}}) => {
+      return Promise.reject(error);
+    },
+);
+
+export default httpClient;
